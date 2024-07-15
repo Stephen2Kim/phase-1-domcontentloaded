2ad6bd9ef4d4435ade305647a502e18678799681
Stephen Muniu Kimani <stvnkimani@gmail.com>
2024-07-15 18:46:39 +0300

    Adds event listener to index.js file

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Target the paragraph element with id="text"
+    const paragraph = document.getElementById('text');
+    
+    // Replace the text with "This is really cool!"
+    paragraph.textContent = "This is really cool!";
+  });
+  
